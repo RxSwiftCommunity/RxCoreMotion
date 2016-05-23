f91af04e1cf0493aa86acfd9926aaced45e75331
Carlos García <carlosypunto@gmail.com>
2016-05-23 23:10:07 +0200

    output to console in all ViewControllers

--- Example/RxCoreMotion_Example/MagnetometerViewController.swift
+++ Example/RxCoreMotion_Example/MagnetometerViewController.swift
@@ -7,29 +7,32 @@
 //
 
 import UIKit
+import RxSwift
+import CoreMotion
+import RxCoreMotion
 
 class MagnetometerViewController: UIViewController {
-
+    
+    let disposeBag = DisposeBag()
+    let coreMotionManager = RxCoreMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        coreMotionManager.magneticField
+            .observeOn(MainScheduler.instance)
+            .subscribeNext { [weak self] magneticField in
+                print(self)
+                print(magneticField)
+            }
+            .addDisposableTo(disposeBag)
+        
     }
-    */
-
+    
 }
+
