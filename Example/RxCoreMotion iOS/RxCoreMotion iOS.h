ebf5036ce173f2437f0761f15762f16039525cda
Kieran Graham <me@kierangraham.com>
2017-03-17 09:11:20 +0100

    Carthage support

---
+++ Example/RxCoreMotion iOS/RxCoreMotion iOS.h
@@ -0,0 +1,19 @@
+//
+//  RxCoreMotion iOS.h
+//  RxCoreMotion iOS
+//
+//  Created by Kieran Graham on 17/03/2017.
+//  Copyright © 2017 RxSwiftCommunity - https://github.com/RxSwiftCommunity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxCoreMotion iOS.
+FOUNDATION_EXPORT double RxCoreMotion_iOSVersionNumber;
+
+//! Project version string for RxCoreMotion iOS.
+FOUNDATION_EXPORT const unsigned char RxCoreMotion_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxCoreMotion_iOS/PublicHeader.h>
+
+
